db13cffed492b50161d995e6e9a315ae81bc9705
Rynav <rynav@rynav.xyz>
2026-03-15 11:45:30 +0100

    fix: Removed broken Query statements from the ConfigRepository.

--- src/main/java/xyz/rynav/openveinsapi/repositories/ConfigRepository.java
+++ src/main/java/xyz/rynav/openveinsapi/repositories/ConfigRepository.java
@@ -8,10 +8,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ConfigRepository extends JpaRepository<Config, String> {
-    @Query("select c.config_name, c.config_value from Config c where c.config_name = ?1")
     Optional<Config> findByConfigName(String configName);
-
-    @Query("select c.config_name, c.config_value from Config c")
-    List<Config> findAllConfig();
-
 }
